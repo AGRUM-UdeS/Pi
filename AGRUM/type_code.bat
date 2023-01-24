2fcc6e328177d8dae7b6e2ff6f540964a7d25377
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-01-24 10:47:46 -0500

    Add prezi url et .bat pour environnement
    S7-91

---
+++ AGRUM/type_code.bat
@@ -0,0 +1,5 @@
+@echo off
+
+rem Open the Developer Command Prompt for VS 2022
+echo Opening the Developer Command Prompt for VS 2022
+start "" "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
