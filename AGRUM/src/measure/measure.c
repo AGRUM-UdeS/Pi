3c78427c8f22f49757e8fe6ea6be0e8e84e3e770
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-01-31 22:49:25 -0500

    S7-45 Start the measurements state machine
    State created but nothing iinside

--- AGRUM/src/measure/measure.c
+++ AGRUM/src/measure/measure.c
@@ -2,7 +2,28 @@
 
 // The main state machine dealing with the measurements
 measure_state_t measure_sm(measure_t* measurements) {
-    static measure_state_t measure_state;
+    static measure_state_t measure_state = IDLE;
+
+    switch (measure_state) {
+    case IDLE:
+        // wait for time to measure
+        break;
+
+    case MEASURING:
+        // Take all the measurements
+
+        break;
+
+    case MESURE_DONE:
+        // ??
+
+        break;
+
+    case ERROR:
+        // Manage errors
+
+        break;
+    }
 
 
     return measure_state;
--- AGRUM/src/measure/measure.h
+++ AGRUM/src/measure/measure.h
@@ -1,12 +1,30 @@
 #ifndef MEASURE_H
 #define MEASURE_H
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define NUMBER_OF_LIMIT_SWITCH  8
+#define NUMBER_OF_PV_ARRAY      2
+
+typedef struct data_t {
+  unsigned char* unit;
+  float value;
+} data_t;
+
 typedef struct measure_t {
-  int a;
+  data_t temperature;
+  data_t humidity;
+  uint limit_switch[NUMBER_OF_LIMIT_SWITCH];
+  data_t voltage_pv[NUMBER_OF_PV_ARRAY];
+  data_t current_pv[NUMBER_OF_PV_ARRAY];
 } measure_t;
 
-typedef struct measure_state_t {
-int a;
+typedef enum {
+  IDLE,
+  MEASURING,
+  MESURE_DONE,
+  ERROR
 } measure_state_t;
 
 measure_state_t measure_sm(measure_t* measurements);
