9cde7cf36bb5ff475621df17413f531d67e31540
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-02-27 17:39:12 -0500

    Created read pin function
    S7-109 Send command then read byte from external ADC

--- AGRUM/src/I2C/ADS7828.c
+++ AGRUM/src/I2C/ADS7828.c
@@ -1,1 +1,28 @@
 #include "ADS7828.h"
+
+const absolute_time_t ABSOLUTE_TIME_INITIALIZED_VAR(ADC_i2c_timeout, 10000); // 10 ms
+
+static ADC_status_t return_ADC_status(int value){
+    ADC_status_t rv;
+
+    if (rv == PICO_ERROR_GENERIC) {
+        rv = ADC_error;
+    } else if (rv == PICO_ERROR_TIMEOUT) {
+        rv = ADC_timeout;
+    } else {
+        rv = ADC_ok;
+    }
+    return rv;
+}
+
+ADC_status_t ADC_read_pin(uint8_t address, uint8_t pin_to_read, uint8_t* received_byte){
+    ADC_status_t status = ADC_ok;
+
+    // Send the command byte to the ADC
+    i2c_write_blocking_until(i2c1, address, &pin_to_read, (size_t)1, false, ADC_i2c_timeout);
+    
+    // Read the bytes from the ADC
+    int nb = i2c_read_blocking_until(i2c1, address, received_byte, (size_t)2, false, ADC_i2c_timeout);
+
+    return return_ADC_status(nb);
+}
--- AGRUM/src/I2C/ADS7828.h
+++ AGRUM/src/I2C/ADS7828.h
@@ -1,5 +1,30 @@
 #ifndef ADS7828_H
 #define ADS7828_H
 
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+
+typedef enum _ADC_status_t{
+  ADC_ok,
+  ADC_timeout,
+  ADC_error
+} ADC_status_t;
+
+// Chip address
+#define ADC_address_0 (0x48)
+#define ADC_address_1 (0x49)
+#define ADC_address_2 (0x4A)
+#define ADC_address_3 (0x4B)
+
+// Command byte
+// Single ended and power down between conversion
+#define ADC_read_pin_1 (0x80)
+#define ADC_read_pin_2 (0x90)
+#define ADC_read_pin_3 (0xA0)
+#define ADC_read_pin_4 (0xB0)
+#define ADC_read_pin_5 (0xC0)
+#define ADC_read_pin_6 (0xD0)
+#define ADC_read_pin_7 (0xE0)
+#define ADC_read_pin_8 (0xF0)
 
 #endif
