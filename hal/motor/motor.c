23bdbb24151ab74cd1ed398106f660b1aec89ed6
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-04-01 13:04:26 -0400

    Update motor.c
    Init all pins that control the motor drives. S7-189

--- hal/motor/motor.c
+++ hal/motor/motor.c
@@ -1,9 +1,21 @@
 #include "motor.h"
 
-#define pul_1_pin 12
-#define pul_2_pin 13
-#define pul_3_pin 14
-#define pul_4_pin 15
+#define IO_MOTOR_ADDRESS   (IO_address_2)
+
+#define PUL1_PIN    12
+#define PUL2_PIN    13
+#define PUL3_PIN    14
+#define PUL4_PIN    15
+
+#define DIR_1_PIN   7
+#define DIR_2_PIN   5
+#define DIR_3_PIN   2
+#define DIR_4_PIN   0
+
+#define ENA_1_PIN   6
+#define ENA_2_PIN   4
+#define ENA_3_PIN   3
+#define ENA_4_PIN   1
 
 #define ONE_KHZ 1000
 #define DUTY_CYCLE 50
@@ -14,27 +26,59 @@
 #define STEP_PER_DEGREE ((int)(25600/360))
 #define GEARBOX_RATIO (47)
 
-void init_motor(void) {
+static void init_pul_pins(void)
+{
     // Set pwm pins
-    init_pwm(pul_1_pin);
-    init_pwm(pul_2_pin);
-    init_pwm(pul_3_pin);
-    init_pwm(pul_4_pin);
+    init_pwm(PUL1_PIN);
+    init_pwm(PUL2_PIN);
+    init_pwm(PUL3_PIN);
+    init_pwm(PUL4_PIN);
 
     // Set pwm frequency
-    set_pwm_freq(pul_1_pin ,ONE_KHZ);
-    set_pwm_freq(pul_2_pin ,ONE_KHZ);
-    set_pwm_freq(pul_3_pin ,ONE_KHZ);
-    set_pwm_freq(pul_4_pin ,ONE_KHZ);
+    set_pwm_freq(PUL1_PIN ,ONE_KHZ);
+    set_pwm_freq(PUL2_PIN ,ONE_KHZ);
+    set_pwm_freq(PUL3_PIN ,ONE_KHZ);
+    set_pwm_freq(PUL4_PIN ,ONE_KHZ);
+}
+
+static void init_dir_pins(void)
+{
+    // Set dir pins as output on IO expander
+    uint8_t byte = (1 << DIR_1_PIN) | (1 << DIR_2_PIN)
+                 | (1 << DIR_3_PIN) | (1 << DIR_4_PIN);
+    IO_set_as_output(IO_MOTOR_ADDRESS, byte);
+}
+
+static void init_ena_pins(void)
+{
+    // Clear pin before setting it as output
+    // because default state is HIGH
+    IO_clear_pin(IO_MOTOR_ADDRESS, ENA_1_PIN);
+    IO_clear_pin(IO_MOTOR_ADDRESS, ENA_2_PIN);
+    IO_clear_pin(IO_MOTOR_ADDRESS, ENA_3_PIN);
+    IO_clear_pin(IO_MOTOR_ADDRESS, ENA_4_PIN);
+
+    // Set enable pins as output on IO expander
+    uint8_t byte = (1 << ENA_1_PIN) | (1 << ENA_2_PIN)
+                 | (1 << ENA_3_PIN) | (1 << ENA_4_PIN);
+    IO_set_as_output(IO_MOTOR_ADDRESS, byte);
+}
+
+void init_motor(void)
+{
+    // Init pins to control motor drives
+    init_pul_pins();
+    init_dir_pins();
+    init_ena_pins();
 }
 
 int64_t stop_rotation(__unused alarm_id_t id, __unused void *user_data) {
     // Set all duty cycle to 0
     printf("Stop moving!\n");
-    disable_pwm(pul_1_pin);
-    disable_pwm(pul_2_pin);
-    disable_pwm(pul_3_pin);
-    disable_pwm(pul_4_pin);
+    disable_pwm(PUL1_PIN);
+    disable_pwm(PUL2_PIN);
+    disable_pwm(PUL3_PIN);
+    disable_pwm(PUL4_PIN);
     return 0;
 }
 void rotate_pv(uint16_t angle, bool clockwise) {
@@ -47,10 +91,10 @@ void rotate_pv(uint16_t angle, bool clockwise) {
     
     // Start moving the motor
     printf("Start moving!\n");
-    enable_pwm(pul_1_pin, DUTY_CYCLE);
-    enable_pwm(pul_2_pin, DUTY_CYCLE);
-    enable_pwm(pul_3_pin, DUTY_CYCLE);
-    enable_pwm(pul_4_pin, DUTY_CYCLE);
+    enable_pwm(PUL1_PIN, DUTY_CYCLE);
+    enable_pwm(PUL2_PIN, DUTY_CYCLE);
+    enable_pwm(PUL3_PIN, DUTY_CYCLE);
+    enable_pwm(PUL4_PIN, DUTY_CYCLE);
 
     // Set time to stop moving motor
     add_alarm_in_ms(angle*STEP_PER_DEGREE*GEARBOX_RATIO, stop_rotation, NULL, false);
