1cc564530af4ce357d8613385c37f3167fb0a80b
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-04-10 11:17:24 -0400

    Update test.c
    Deleted specific motor test

--- app/utils/test.c
+++ app/utils/test.c
@@ -4,15 +4,4 @@ void develop_test(void)
 {
     printf("TEST\n");
     // Add function to test here
-    uint16_t m1 = 0;
-    rotate_single_pv(m1, 1, false);
-    sleep_ms(100);
-    uint16_t m2 = 1;
-    rotate_single_pv(m2, 1, false);
-    sleep_ms(100);
-    uint16_t m3 = 2;
-    rotate_single_pv(m3, 1, false);
-    sleep_ms(100);
-    uint16_t m4 = 3;
-    rotate_single_pv(m4, 1, false);
 }
