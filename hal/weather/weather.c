fffa85b746c1b5805c40fc1a55a2bfc8ee73ba29
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-04-19 15:04:52 -0400

    Weather - Mecanism to read mutliple buffer
    Because forecast can be huge buffer sent in multiple packets. S7-160

--- hal/weather/weather.c
+++ hal/weather/weather.c
@@ -2,17 +2,21 @@
 
 #define HTTP_PORT   HTTP_DEFAULT_PORT
 #define BUF_SIZE 2048
+#define BUF_NB   4
 
 static bool weather_is_received = false;
-static char myBuff[BUF_SIZE] = {0};
+static bool new_weather_request = false;
+
+static char myBuff[BUF_NB][BUF_SIZE] = {0};
+static uint8_t myBuff_index = 0;
 
 const char* WEATHER_REQUEST = 
 "api.open-meteo.com";
 
 const char* MONTREAL_WEATHER = 
-"/v1/forecast?latitude=45.53&longitude=-73.52&hourly=rain,cloudcover,windspeed_80m,direct_radiation_instant&timeformat=unixtime&forecast_days=1&timezone=America%2FNew_York";
+"/v1/forecast?latitude=45.53&longitude=-73.52&hourly=rain,cloudcover,windspeed_80m,direct_radiation_instant&timeformat=unixtime&forecast_days=3&timezone=America%2FNew_York";
 
-#define END_OF_MSG ("]}}")
+const char* END_OF_MSG = "]}}";
 
 void httpc_result_cb(void *arg, httpc_result_t httpc_result, 
                         u32_t rx_content_len, u32_t srv_res, err_t err) {
@@ -35,13 +39,24 @@ err_t httpc_headers_cb(httpc_state_t *connection, void *arg,
 }
 
 err_t httpc_body_cb(void *arg, struct altcp_pcb *conn, struct pbuf *p, err_t err) {
-    // Save the weather buffer in a global variable
-    pbuf_copy_partial(p, myBuff, p->tot_len, 0);
+    if (new_weather_request != true) {
+        return ERR_OK;
+    }
 
-    char *ptr = strstr(myBuff, END_OF_MSG);
+    // Save the weather buffer in a global variable
+    static uint8_t i = 0;
+    pbuf_copy_partial(p, myBuff[i], p->tot_len, 0);
+
+    char *ptr = NULL;
+    ptr = strstr(myBuff[i], END_OF_MSG);
     if (ptr != NULL) {
         // Tell the application data has been received
         weather_is_received = true;
+        new_weather_request = false;
+        myBuff_index = i;
+        i = 0;
+    } else {
+        i++;
     }
     return ERR_OK;
 }
@@ -50,13 +65,21 @@ void weather_current_request(void) {
     httpc_connection_t settings;
     settings.result_fn = httpc_result_cb;
     settings.headers_done_fn = httpc_headers_cb;
-    
+
+    new_weather_request = true;
     httpc_get_file_dns(WEATHER_REQUEST, HTTP_PORT, MONTREAL_WEATHER, &settings, httpc_body_cb, NULL, NULL);
     
     while (!weather_is_received) {
         tight_loop_contents();
     }
-    printf("Body : %s\n", myBuff);
+    weather_is_received = false;
+
+    printf("Body (%d) :\n", myBuff_index);
+    for (size_t i = 0; i <= myBuff_index; i++)
+    {
+        printf("%s", myBuff[i]);
+    }
+    printf("\n");
 }
 
 void print_current_weather(void) {
