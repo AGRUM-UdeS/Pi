6f0399725b83eefb3024240413c7ff855188dd60
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-04-19 14:03:09 -0400

    Remove weather request
    Since now everything is in one request. S7-160

--- hal/weather/weather.c
+++ hal/weather/weather.c
@@ -2,14 +2,9 @@
 
 #define HTTP_PORT   HTTP_DEFAULT_PORT
 #define BUF_SIZE 2048
-#define BUF_NB  16
 
 static bool weather_is_received = false;
-static bool weather_forecast_is_received = false;
-static ip_addr_t weather_ipaddr;
-static struct pbuf* weather_forecast_buf[BUF_NB];
 static char myBuff[BUF_SIZE] = {0};
-static uint16_t nb_packet = 0;
 
 const char* WEATHER_REQUEST = 
 "api.open-meteo.com";
@@ -37,18 +32,6 @@ err_t httpc_headers_cb(httpc_state_t *connection, void *arg,
     return ERR_OK;
 }
 
-err_t forecast_cb(void *arg, struct altcp_pcb *conn, struct pbuf *p, err_t err) {
-    // Save the weather buffer in a global variable
-    weather_forecast_buf[nb_packet++] = p;
-    if (nb_packet >= BUF_NB) {
-        nb_packet = 0;
-    }
-    // Tell the application data has been received
-    weather_forecast_is_received = true;
-    
-    return ERR_OK;
-}
-
 err_t httpc_body_cb(void *arg, struct altcp_pcb *conn, struct pbuf *p, err_t err) {
     // Save the weather buffer in a global variable
     pbuf_copy_partial(p, myBuff, p->tot_len, 0);
@@ -56,16 +39,6 @@ err_t httpc_body_cb(void *arg, struct altcp_pcb *conn, struct pbuf *p, err_t err
     weather_is_received = true;
     
     return ERR_OK;
-}
-
-void weather_forecast_request(void) {
-    httpc_connection_t settings;
-    settings.result_fn = httpc_result_cb;
-    settings.headers_done_fn = httpc_headers_cb;
-
-    weather_forecast_is_received = false;
-
-    httpc_get_file_dns(WEATHER_REQUEST, HTTP_PORT, MONTREAL_WEATHER_FORECAST, &settings, forecast_cb, NULL, NULL);
 }
 
 void weather_current_request(void) {
@@ -79,36 +52,7 @@ void weather_current_request(void) {
     while (!weather_is_received) {
         tight_loop_contents();
     }
-
-    // printf("Body received (%d), %x, %x\n", buf->tot_len, buf->type_internal, buf->flags);
     printf("Body : %s\n", myBuff);
-    // while (buf->next) {
-    //     buf = buf->next;
-    //     printf("Body : %s\n", buf->payload);
-    // }
-    // printf("\nFINISH\n");
-}
-
-void print_weather_forecast(void) {
-    static uint16_t i = 0;
-
-    if ((!weather_forecast_is_received) || ((i >= nb_packet) && (nb_packet != 0))) {
-        return;
-    }
-
-    struct pbuf* buf = weather_forecast_buf[i++];
-
-    char* ptr = strstr(buf->payload, "\"temp\":");
-    char num[] = {(char)(*(ptr+7)), (char)(*(ptr+8)), (char)(*(ptr+9)), 
-                    (char)(*(ptr+10)), (char)(*(ptr+11)), (char)(*(ptr+12)), '\0'};
-    printf("\n\nFirst temp is %s", num);
-    printf(" => %f\n\n", atof(num));
-
-    printf("%s", buf->payload);
-
-    if (i >= BUF_NB) {
-        i = 0;
-    }
 }
 
 void print_current_weather(void) {
