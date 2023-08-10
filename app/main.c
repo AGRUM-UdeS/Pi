2aaac42736a7da3be040a9b2e3c2a85eab60467f
Alexis Gendron <alexis.gendron@usherbrooke.ca>
2023-08-09 20:55:21 -0700

    T-22 Add FreeRTOS support

--- app/main.c
+++ app/main.c
@@ -4,6 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "interface.h"
@@ -15,6 +20,13 @@
 
 #include "weather.h"
 
+/* Prototypes for the standard FreeRTOS callback/hook functions implemented
+within this file. */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+
 void init(void) {
     // Init RP2040 peripherals
     stdio_init_all();
@@ -22,13 +34,12 @@ void init(void) {
     // Delay to let the developer open Putty
     usb_delay(5);
 
-    // Init watchdog before wifi connection
-    init_watchdog();
-    
     if (connect_to_interface() == INTERFACE_CONNECTED) {
         init_timer();
         interface_publish(PI_STATUS_TOPIC, PI_STATUS_CONNECTED);
     }
+    // Init watchdog before wifi connection
+    init_watchdog();
 
     feed_watchdog();
 
@@ -66,4 +77,82 @@ int main() {
 
     }
     return 0;
+}
+
+void vApplicationMallocFailedHook( void )
+{
+    /* Called if a call to pvPortMalloc() fails because there is insufficient
+    free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+    internally by FreeRTOS API functions that create tasks, queues, software
+    timers, and semaphores.  The size of the FreeRTOS heap is set by the
+    configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+    volatile size_t xFreeHeapSpace;
+
+    /* This is just a trivial example of an idle hook.  It is called on each
+    cycle of the idle task.  It must *NOT* attempt to block.  In this case the
+    idle task just queries the amount of FreeRTOS heap that remains.  See the
+    memory management section on the http://www.FreeRTOS.org web site for memory
+    management options.  If there is a lot of heap memory free then the
+    configTOTAL_HEAP_SIZE value in FreeRTOSConfig.h can be reduced to free up
+    RAM. */
+    xFreeHeapSpace = xPortGetFreeHeapSize();
+
+    /* Remove compiler warning about xFreeHeapSpace being set but never used. */
+    ( void ) xFreeHeapSpace;
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+#if mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 0
+    {
+        /* The full demo includes a software timer demo/test that requires
+        prodding periodically from the tick interrupt. */
+        #if (mainENABLE_TIMER_DEMO == 1)
+        vTimerPeriodicISRTests();
+        #endif
+
+        /* Call the periodic queue overwrite from ISR demo. */
+        #if (mainENABLE_QUEUE_OVERWRITE == 1)
+        vQueueOverwritePeriodicISRDemo();
+        #endif
+
+        /* Call the periodic event group from ISR demo. */
+        #if (mainENABLE_EVENT_GROUP == 1)
+        vPeriodicEventGroupsProcessing();
+        #endif
+
+        /* Call the code that uses a mutex from an ISR. */
+        #if (mainENABLE_INTERRUPT_SEMAPHORE == 1)
+        vInterruptSemaphorePeriodicTest();
+        #endif
+
+        /* Call the code that 'gives' a task notification from an ISR. */
+        #if (mainENABLE_TASK_NOTIFY == 1)
+        xNotifyTaskFromISR();
+        #endif
+    }
+#endif
 }
